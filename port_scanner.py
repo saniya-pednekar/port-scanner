8bc0bf9aa8303b36638f7733470359756aa677e7
saniya-pednekar <sani.pednekar09@gmail.com>
2026-02-07 13:06:57 +0530

    Upgrade scanner with service detection and error handling
    Smart Python port scanner with service detection for ethical hacking practice.

--- port_scanner.py
+++ port_scanner.py
@@ -2,16 +2,40 @@ import socket
 
 target = input("Enter target IP or website: ").strip()
 
-print("Scanning target:", target)
+try:
+    target_ip = socket.gethostbyname(target)
+except socket.gaierror:
+    print("Invalid host or network issue.")
+    exit()
+
+print("\nScanning target:", target_ip)
+print("Please wait...\n")
+
+common_ports = {
+    21: "FTP",
+    22: "SSH",
+    23: "Telnet",
+    25: "SMTP",
+    53: "DNS",
+    80: "HTTP",
+    110: "POP3",
+    135: "RPC",
+    139: "NetBIOS",
+    143: "IMAP",
+    443: "HTTPS",
+    445: "SMB"
+}
 
 for port in range(20, 1025):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.settimeout(0.5)
+    s.settimeout(0.3)
 
-    result = s.connect_ex((target, port))
+    result = s.connect_ex((target_ip, port))
 
     if result == 0:
-        print("Port", port, "is OPEN")
+        service = common_ports.get(port, "Unknown Service")
+        print(f"Port {port} OPEN ({service})")
 
     s.close()
 
+print("\nScan complete.")
